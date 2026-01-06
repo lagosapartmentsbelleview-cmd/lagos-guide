28cb8134a68d9ebeb1d4ceb99e7cd06e63365ebe
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-01-06 23:07:34 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,8 @@
-// Import Firebase SDKs (versão compatível com <script>)
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// A tua configuração Firebase
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyD70hv-PgWJKDbK-tcxeRuB1FE1cVs-FBs",
   authDomain: "reservas-marina-park.firebaseapp.com",
   projectId: "reservas-marina-park",
@@ -13,9 +12,5 @@ export const firebaseConfig = {
   measurementId: "G-14GR64WXHS"
 };
 
-// Inicializar Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Inicializar Firestore
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
