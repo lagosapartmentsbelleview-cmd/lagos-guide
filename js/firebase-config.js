dd8387a494c5e8d2fa541b3787453e832ee12063
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-01-07 00:46:10 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,7 @@
 // firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
+// Configuração do Firebase
+var firebaseConfig = {
   apiKey: "AIzaSyD70hv-PgWJKDbK-tcxeRuB1FE1cVs-FBs",
   authDomain: "reservas-marina-park.firebaseapp.com",
   projectId: "reservas-marina-park",
@@ -12,5 +11,8 @@ const firebaseConfig = {
   measurementId: "G-14GR64WXHS"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = firebase.firestore();
