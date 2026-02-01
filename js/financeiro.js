34fe9132619ca685484e86d6856aab52a3046958
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-02-01 16:30:28 +0000

    Update financeiro.js

--- js/financeiro.js
+++ js/financeiro.js
@@ -193,7 +193,7 @@ async function carregarFaturas() {
     try {
         const snap = await firebase.firestore()
             .collection("faturas")
-            .orderBy("dataISO")   // ← vamos ordenar pelo campo correto
+            .orderBy("data")
             .get();
 
         faturasCache = snap.docs.map(d => {
