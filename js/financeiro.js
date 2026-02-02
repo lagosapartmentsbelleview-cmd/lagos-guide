ee8a8039ee1e25a118370f31856234701c36500d
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-02-02 00:22:46 +0000

    Update financeiro.js

--- js/financeiro.js
+++ js/financeiro.js
@@ -327,8 +327,9 @@ function entrarModoEdicao(id, botao) {
             td.innerHTML = gerarSelectCategorias(valor);
         } else if (campo === "fornecedor") {
             td.innerHTML = gerarSelectEntidades(valor);
-        } else if (campo === "dataISO") {
-            td.innerHTML = `<input type="date" value="${valor}">`;
+       } else if (campo === "dataISO") {
+    td.innerHTML = `<input type="date" value="${f.dataISO || ""}">`;
+}
         } else {
             td.innerHTML = `<input type="text" value="${valor}">`;
         }
