d70fe61482856be775406370e89d959c06e49c37
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-02-10 12:22:47 +0000

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,4 @@
+document.getElementById("btnDisponibilidade").addEventListener("click", () => {
+    const res = document.getElementById("resultadoDisponibilidade");
+    res.innerHTML = "<p>Disponibilidade será carregada aqui…</p>";
+});
