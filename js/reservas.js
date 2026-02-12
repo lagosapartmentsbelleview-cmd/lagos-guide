8398b063ad2ed014566849535e0de7b98dc55e77
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-02-12 13:40:30 +0000

    Update reservas.js

--- js/reservas.js
+++ js/reservas.js
@@ -358,18 +358,7 @@ btn.addEventListener("click", () => {
 
 });
 
-// Expansão mobile do card de disponibilidade
-document.addEventListener("click", function (e) {
-    const card = document.getElementById("resultadoDisponibilidade");
 
-    if (!card.classList.contains("has-result")) return; // só funciona se houver resultado
-
-    if (window.innerWidth <= 768) {
-        if (e.target.closest("#resultadoDisponibilidade")) {
-            card.classList.toggle("expandido");
-        }
-    }
-});
 
 // ---------------------------------------------------------------
 // EVENTOS
