8310effcf7428ab1d05591d8c9f80084be35ef81
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-02-20 23:30:15 +0000

    Update guide.js

--- js/guide.js
+++ js/guide.js
@@ -2309,10 +2309,11 @@ async function openInternalMap(url) {
       leafletMap = L.map('leafletMap').setView([lat, lon], 16);
 
       // Adicionar tiles OSM
-      L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       maxZoom: 19,
-      attribution: 'Tiles © Esri — Source: Esri, DeLorme, NAVTEQ'
+      attribution: '&copy; OpenStreetMap & Carto'
     }).addTo(leafletMap);
+
 
 
       // Marcador
