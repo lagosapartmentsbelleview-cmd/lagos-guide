685dfd1baacce518f7d695182c81ad2c28e88c8d
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-03-20 13:37:40 +0000

    Update operacional.js

--- js/operacional.js
+++ js/operacional.js
@@ -1,3 +1,9 @@
+firebase.auth().onAuthStateChanged(user => {
+    if (!user) {
+        window.location.href = "login.html";
+    }
+});
+
 // ============================================================
 // 0) VARIÁVEIS GLOBAIS
 // ============================================================
