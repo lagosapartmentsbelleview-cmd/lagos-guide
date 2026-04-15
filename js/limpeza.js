328b1142272239ad7d6d485d92fc7b549fbbf436
lagosapartmentsbelleview-cmd <lagosapartmentsbelleview@gmail.com>
2026-04-16 00:08:36 +0100

    Update limpeza.js

--- js/limpeza.js
+++ js/limpeza.js
@@ -259,7 +259,7 @@ Obs: ${r.comentarios || "-"}
                     div.classList.add("reserva-meio");
                 }
 
-                div.setAttribute("data-info", tooltipTexto);
+                cel.setAttribute("data-info", tooltipTexto);
                 cel.appendChild(div);
 
                 // marcar célula como tendo reserva (para o CSS da grelha)
